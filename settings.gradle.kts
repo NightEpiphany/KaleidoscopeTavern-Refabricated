707866898f0d30d2bf6bdcee07878a97175ea188
NightEpiphany <126158635+NightEpiphany@users.noreply.github.com>
2026-09-12 21:06:32 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 	plugins {
 		id("net.fabricmc.fabric-loom") version providers.gradleProperty("loom_version")
 	}
-
-	// Title for the project
-	rootProject.name = "KT-26.2-fabric"
 }
+
+// Title for the project
+rootProject.name = "KT-26.2-fabric"
